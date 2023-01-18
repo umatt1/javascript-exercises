40320c8cd149ef772090914c3a4168536d0749f6
umatt1 <matt.burkhard@gmail.com>
2023-01-18 18:48:29 -0500

    Implements exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(dig1=0,dig2=0) {
+	return dig1+dig2;
 };
 
-const subtract = function() {
-	
+const subtract = function(dig1=0,dig2=0) {
+	return dig1-dig2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(accumulator, current){return accumulator+current},0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(function(accumulator,current) {return accumulator*current}, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
