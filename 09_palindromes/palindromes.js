78248afef8e77a80e5be9d9d7becfbacdc129b6d
umatt1 <matt.burkhard@gmail.com>
2023-01-18 19:11:56 -0500

    Implements exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    myString = myString.toLowerCase().replace(/[^\w]/g, "");
+    let i = 0;
+    let j = myString.length-1;
+    while (i < j && myString.charAt(i)==myString.charAt(j)) {
+        i++;
+        j--;
+    }
+    return i >= j;
 };
-
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
